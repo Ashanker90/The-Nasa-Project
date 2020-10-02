9a4b33387ad99778b818a9d7d56d9188d994d369
Gabriella Pensa <gpensa@luc.edu>
2020-10-01 19:03:07 -0500

    Update asteroids

---
+++ assets/asteroids.js
@@ -0,0 +1,18 @@
+var req = new XMLHttpRequest();
+var url = "https://api.nasa.gov/neo/rest/v1/feed?start_date=START_DATE&end_date=END_DATE&api_key=API_KEY";
+var api_key = "wGXhi7fLKdBmDP5PdNR5Eu3N4JOvJ9lez2UnnqwJ";
+
+req.open("GET", url + api_key);
+req.send();
+
+req.addEventListener("load", function(){
+	if(req.status == 200 && req.readyState == 4){
+  	var response = JSON.parse(req.responseText);
+    document.getElementById("name").textContent = response.name;
+		document.getElementById("id").textContent = response.id;
+		document.getElementById("neo_reference_id").textContent = response.neo_reference_id;
+    document.getElementById("is_potentially_hazardous_asteroid").textContent = response.is_potentially_hazardous_asteroid;
+    document.getElementById("close_approach_data").src = response.close_approach_data;
+    document.getElementById("orbital_data").textContent = response.orbital_data;
+  }
+})
