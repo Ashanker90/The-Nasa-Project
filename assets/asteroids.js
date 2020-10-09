54a951a65b49d5e63d062027c011a5d27df99cac
kellmerrell <kmerrell@luc.edu>
2020-10-09 13:17:52 -0500

    made changes to asteroids.js

--- assets/asteroids.js
+++ assets/asteroids.js
@@ -1,54 +1,41 @@
-//var today = new Date();
-//var start_date = today.getFullYear()+'-'+(today.getMonth()+1)+'-'+today.getDate();
-
-//var req = new XMLHttpRequest();
-//var url = `https://api.nasa.gov/neo/rest/v1/feed?start_date=${start_date}&api_key=`;
-//var api_key = "wGXhi7fLKdBmDP5PdNR5Eu3N4JOvJ9lez2UnnqwJ";
-
 const req = new XMLHttpRequest();
 var today = new Date();
-var start_date = today.getFullYear()+'-'+(today.getMonth()+1)+'-'+today.getDate();
+var start_date = new Date().toISOString().slice(0,10);
 const api_key = "wGXhi7fLKdBmDP5PdNR5Eu3N4JOvJ9lez2UnnqwJ";
-const url = `https://api.nasa.gov/neo/rest/v1/feed?start_date=${start_date}&end_date=${end_date}&api_key=`;
+const url = `https://api.nasa.gov/neo/rest/v1/feed?start_date=${start_date}&api_key=${api_key}`;
 
 req.open("GET", url);
 req.send();
 
-req.addEventListener("load", function(){
-	if(req.status == 200 && req.readyState == 4){
-  	var response = JSON.parse(req.responseText);
-		var start_date = new Date().toISOString().slice(0,10);
-		function(data)
+req.addEventListener("load", function() {
+	if(req.status == 200 && req.readyState == 4) {
+		var response = JSON.parse(req.responseText);
+		var asteroids = response.near_earth_objects[start_date];
+
+		if(asteroids.length==0)
 		{
-			var object = data;
-		  var asteroids = object.near_earth_objects;
-
-			if(asteroids.length==0)
+			body: "No asteroids for today.";
+		} else {
+			try
 			{
-		      body: "No asteroids for today.";
-		  }
-			else {
-		          try
-							{
-		            for(var i = 0; i < asteroids[todayDate].length; i++)
-								{
-		              var obj = asteroids[todayDate][i];
-									var names = obj.name;
-		              var lunarDistance = obj.close_approach_data[0].miss_distance.lunar;
-		              var id = obj.id[0];
-									var neo_reference_id = obj.neo_reference_id;
-									var hazard = obj.is_potentially_hazardous_asteroid;
-									var close = obj.close_approach_data;
-									var orbit = obj.orbital_data;
-		            }
-		          }
-
-							catch(err)
-							{
-		            console.log('Caught error' + err);
-		          }
-						}
+				for(var i = 0; i < asteroids.length; i++) {
+					alert(asteroids[i].name);
+					// var obj = asteroids[todayDate][i];
+					// var names = obj.name;
+					// var lunarDistance = obj.close_approach_data[0].miss_distance.lunar;
+					// var id = obj.id[0];
+					// var neo_reference_id = obj.neo_reference_id;
+					// var hazard = obj.is_potentially_hazardous_asteroid;
+					// var close = obj.close_approach_data;
+					// var orbit = obj.orbital_data;
+				}
 			}
+				catch(err)
+			{
+				console.log('Caught error' + err);
+			}
+		}
+	}
 
 //		document.getElementById("today-date").textContent = "start_date";
 //		document.getElementById("in-date").textContent = response.start_date;
@@ -58,5 +45,5 @@ req.addEventListener("load", function(){
   //  document.getElementById("is_potentially_hazardous_asteroid").textContent = response.is_potentially_hazardous_asteroid;
   //  document.getElementById("close_approach_data").src = response.close_approach_data;
   //  document.getElementById("orbital_data").textContent = response.orbital_data;
-  }
+
 })
