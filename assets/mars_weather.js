d6ee616491b04f6d58e88ee6e99b0f4d6438fcf3
kelleymerrell <kmerrell@luc.edu>
2020-10-11 12:57:23 -0500

    Made some small changes to Mars

--- assets/mars_weather.js
+++ assets/mars_weather.js
@@ -1,6 +1,6 @@
-var req = new XMLHttpRequest();
-var api_key = "9y3Tse8OLkkMvf3FEoY9Y17IcK2u33hWMhGg5ubR";
-var url = `https://api.nasa.gov/insight_weather/?api_key=${api_key}&feedtype=json&ver=1.0`;
+let req = new XMLHttpRequest();
+let api_key = "9y3Tse8OLkkMvf3FEoY9Y17IcK2u33hWMhGg5ubR";
+let url = `https://api.nasa.gov/insight_weather/?api_key=${api_key}&feedtype=json&ver=1.0`;
 
 req.open("GET", url);
 req.send();
@@ -9,19 +9,20 @@ req.addEventListener("load", function() {
     if (req.status == 200 && req.readyState == 4) {
         const response = JSON.parse(req.responseText);
         const sol = response.sol_keys[6];
-        document.getElementById("header").textContent = "Weather on Mars";
+        document.getElementById("header").textContent = "Welcome to Mars!";
+        document.getElementById("header2").textContent = "Today's Weather";
         document.getElementById("mars-date").textContent = "SOL " + response.sol_keys[6];
         date = new Date(response[sol].First_UTC);
-        var month = date.toLocaleString("en-us", { month: "short" });
-        var day = date.getDate();
-        var year = date.getFullYear();
-        var newDate = month +' ' + day +' ' + year
+        let month = date.toLocaleString("en-us", { month: "short" });
+        let day = date.getDate();
+        let year = date.getFullYear();
+        let newDate = month +' ' + day +' ' + year
         document.getElementById("earth-date").textContent = newDate;
         document.getElementById("season").textContent = "Season: " + response[sol].Season;
         document.getElementById("temp-high").textContent = "High: " + response[sol].AT.mx;
         document.getElementById("temp-low").textContent = "Low: " + response[sol].AT.mn;
         document.getElementById("wind-direction").textContent = "Wind Direction: " + response[sol].WD.most_common.compass_point;
-        document.getElementById("wind-speed").textContent = "Wind Speed: " + response[sol].HWS.mx;
+        document.getElementById("wind-speed").textContent = "Wind Speed: " + response[sol].HWS.mx + " (m/s)";
 
         buildTable(response);
     }
@@ -29,20 +30,20 @@ req.addEventListener("load", function() {
 
 function buildTable(data) {
     const solKeys =  data.sol_keys;
-    var table = document.getElementById("sol-table-body");
+    let table = document.getElementById("sol-table-body");
 
-    var solRow = "<tr class='sol-table-body-row'/>";
-    var earthDateRow = "<tr class='sol-table-body-row'/>";
-    var highTempRow = "<tr class='sol-table-body-row'/>";
-    var lowTempRow = "<tr class='sol-table-body-row'/>";
+    let solRow = "<tr class='sol-table-body-row'/>";
+    let earthDateRow = "<tr class='sol-table-body-row'/>";
+    let highTempRow = "<tr class='sol-table-body-row'/>";
+    let lowTempRow = "<tr class='sol-table-body-row'/>";
 
-    for (var i = 0; i < solKeys.length; i++) {
+    for (let i = 0; i < solKeys.length; i++) {
         const sol = solKeys[i];
         date = new Date(data[sol].First_UTC);
-        var month = date.toLocaleString("en-us", { month: "short" });
-        var day = date.getDate();
-        var year = date.getFullYear();
-        var newDate = month +' ' + day +' ' + year
+        let month = date.toLocaleString("en-us", { month: "short" });
+        let day = date.getDate();
+        let year = date.getFullYear();
+        let newDate = month +' ' + day +' ' + year
 
         solRow += "<td>" + sol + "</td>";
         earthDateRow += "<td>" + newDate + "</td>";
