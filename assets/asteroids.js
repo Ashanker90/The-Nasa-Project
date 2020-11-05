f5cb6de63caaec2546036a416a3dafba5ba7f5b1
kelleymerrell <kmerrell@luc.edu>
2020-11-04 20:15:08 -0600

    Made changes to asteroid code

--- assets/asteroids.js
+++ assets/asteroids.js
@@ -1,19 +1,22 @@
-let req = new XMLHttpRequest();
-
+const req = new XMLHttpRequest();
 let today = new Date();
-let start_date = today.getFullYear() + "-" + (today.getMonth() + 1) + "-" + today.getDate();
-var date = start_date.toString();
+//let start_date = today.getFullYear() + "-" + (today.getMonth() + 1) + "-" + today.getDate();
+
+let start_date = today
+    .toLocaleString('en-us', {year: 'numeric', month: '2-digit', day: '2-digit'})
+    .replace(/(\d+)\/(\d+)\/(\d+)/, '$3-$1-$2');
+
+// let date = start_date.toString();
 //makes a second object date
-var second = new Date();
+let second = new Date();
 //finds the seven days before
-var before = second.getDate() + 7;
+let before = second.getDate() + 7;
 //sets the second date to 7 days before the current date
 second.setDate(before);
-var sevenStr = second.toString();
-
+let sevenStr = second.toString();
 
 const api_key = "wGXhi7fLKdBmDP5PdNR5Eu3N4JOvJ9lez2UnnqwJ";
-const url = `https://api.nasa.gov/neo/rest/v1/feed?start_date=${start_date}&api_key=${api_key}`;
+const url = `https://api.nasa.gov/neo/rest/v1/feed?start_date=${start_date}&end_date=${start_date}&api_key=${api_key}`;
 
 req.open("GET", url);
 req.send();
@@ -25,16 +28,12 @@ req.addEventListener("load", function () {
     //the line below gets the current date of the device
     document.getElementById("start_date").innerHTML = today;
     document.getElementById("second").innerHTML = second;
-    //let asteroids = response.near_earth_objects[start_date];
     //takes the stuff from the response and makes it a string
     let stuff = JSON.stringify(response);
     //outputs what is being responded
     //document.getElementById('length').innerHTML = length;
-    document.getElementById('stuff').innerHTML = stuff;
+    // document.getElementById('stuff').innerHTML = stuff;
     //makeNearEarth(stuff);
-
-
-
 
     let asteroids = response.near_earth_objects[start_date];
     if (asteroids == undefined || asteroids.length == 0) {
@@ -42,29 +41,32 @@ req.addEventListener("load", function () {
       createListItem("No asteroids for today");
     } else {
       try {
-        //  for (var i = 0; i < response.length; i++) {
-      //  asteroids.push(response);
-      //  createListItem(asteroids[i].name);
-        // var obj = asteroids[todayDate][i];
-        // var names = obj.name;
-        // var lunarDistance = obj.close_approach_data[0].miss_distance.lunar;
-        // var id = obj.id[0];
-        // var neo_reference_id = obj.neo_reference_id;
-        // var hazard = obj.is_potentially_hazardous_asteroid;
-        // var close = obj.close_approach_data;
-        // var orbit = obj.orbital_data;
-    //  }
+        for (var i = 0; i < asteroids.length; i++) {
+          // asteroids.push(response);
+          // createListItem(asteroids[i].name);
+          // var obj = asteroids[todayDate][i];
+
+          var name = asteroids[i].name;
+          alert("NAME: " + name);
+
+          // var lunarDistance = obj.close_approach_data[0].miss_distance.lunar;
+          // var id = obj.id[0];
+          // var neo_reference_id = obj.neo_reference_id;
+          // var hazard = obj.is_potentially_hazardous_asteroid;
+          // var close = obj.close_approach_data;
+          // var orbit = obj.orbital_data;
+        }
       } catch (err) {
         console.log("Caught error" + err);
       }
     }
   }
 
-  //		document.getElementById("today-date").textContent = "start_date";
-  //		document.getElementById("in-date").textContent = response.start_date;
+  //  document.getElementById("today-date").textContent = "start_date";
+  //  document.getElementById("in-date").textContent = response.start_date;
   //  document.getElementById("name").textContent = response.name;
-  //	document.getElementById("id").textContent = response.id;
-  //	document.getElementById("neo_reference_id").textContent = response.neo_reference_id;
+  //  document.getElementById("id").textContent = response.id;
+  //  document.getElementById("neo_reference_id").textContent = response.neo_reference_id;
   //  document.getElementById("is_potentially_hazardous_asteroid").textContent = response.is_potentially_hazardous_asteroid;
   //  document.getElementById("close_approach_data").src = response.close_approach_data;
   //  document.getElementById("orbital_data").textContent = response.orbital_data;
