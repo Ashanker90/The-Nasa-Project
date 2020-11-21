425017ceaec5d9b7bba21a81e0f5d37415d0ed1b
gpensa <gpensa@luc.edu>
2020-11-21 16:53:39 -0600

    mini calanders created, working on columns (asteroid code)

--- assets/asteroids.js
+++ assets/asteroids.js
@@ -5,7 +5,9 @@ let today = new Date();
 let start_date = today
     .toLocaleString('en-us', {year: 'numeric', month: '2-digit', day: '2-digit'})
     .replace(/(\d+)\/(\d+)\/(\d+)/, '$3-$1-$2');
-
+let months = ["Jan", "Feb", "March", "April", "May", "June", "July", "August", "Sept", "Oct", "Nov", "Dec"];
+let monthWord = months[today.getMonth()];
+//let start_month = today.toLocaleString('en-us', { month: '2-digit' });
 // let date = start_date.toString();
 //makes a second object date
 let second = new Date();
@@ -14,6 +16,7 @@ let before = second.getDate() + 7;
 //sets the second date to 7 days before the current date
 second.setDate(before);
 let sevenStr = second.toString();
+let monthSec = months[second.getMonth()];
 
 const api_key = "wGXhi7fLKdBmDP5PdNR5Eu3N4JOvJ9lez2UnnqwJ";
 const url = `https://api.nasa.gov/neo/rest/v1/feed?start_date=${start_date}&end_date=${start_date}&api_key=${api_key}`;
@@ -26,9 +29,13 @@ req.addEventListener("load", function () {
     let response = JSON.parse(req.responseText);
     //let length = response.near_earth_objects.length - 1;
     //the line below gets the current date of the device
-    document.getElementById("start_date").innerHTML = today;
-    document.getElementById("second").innerHTML = second;
-
+    //document.getElementById("start_date").innerHTML = today;
+    // document.getElementById("second").innerHTML = second;
+    //  document.getElementById("start_month").innerHTML = (today.getMonth() + 1);
+    document.getElementById("start_month").innerHTML = monthWord;
+    document.getElementById("start_day").innerHTML = today.getDate();
+    document.getElementById("end_month").innerHTML = monthSec;
+    document.getElementById("end_day").innerHTML = second.getDate();
     //takes the stuff from the response and makes it a string
     let stuff = JSON.stringify(response);
     //outputs what is being responded
