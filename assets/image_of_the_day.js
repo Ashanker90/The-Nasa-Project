24ca3ac2d3638da7b68cef32034284e9de27b6f3
Anshul Shanker <anshulshankar@gmail.com>
2020-11-30 20:41:20 -0600

    Add logic to handle videos and images

--- assets/image_of_the_day.js
+++ assets/image_of_the_day.js
@@ -17,12 +17,25 @@ async function getResponse(url) {
 }
 
 function createElements(response) {
-  // debugger;
   document.getElementById("title").textContent = response.title;
   document.getElementById("date").textContent = response.date;
+  if (response.media_type === "image") {
+    document.getElementById("pic").src = response.hdurl;
+    sessionStorage.setItem("mediaType", "image");
+  } else {
+    document.getElementById("videos").src = response.hdurl;
+  }
   document.getElementById("pic").src = response.hdurl;
-  // document.getElementById("pic").src = response.url;
   document.getElementById("explanation").textContent = response.explanation;
 }
 
 getResponse(url);
+
+function showMedia() {
+  if (sessionStorage.getItem("mediaType") === "image") {
+    document.getElementById("video").style.display = "none";
+  }
+  else {
+    document.getElementById("img").style.display = "none";
+  }
+}
